fa749df05a957c3f3dff6d77d6e9667ee1ed3213
resufer <66846932+resufer@users.noreply.github.com>
2022-02-23 00:27:39 +0300

    Add IIFE with first example

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ let execute = (command) => {
         if (stderr) console.log(chalk.hex('#FD42B9')(`error in command console: ${stderr}`));
     })
 };
+
+(() => {
+    let arg = process.argv[2];
+    let example;
+    if (!arg) {
+        console.log('this script requires arguments: example1 | example2 | ... | example10');
+        return;
+    } else if (arg === 'example1') {
+        example = [
+            'git init',
+            'node ./fH/fileHandler.js "git init"',
+            'git add .',
+            'git commit -m "some"',
+            'node ./fH/fileHandler.js "git commit -m \'some\'"',
+            'git branch br2',
+            'node ./fH/fileHandler.js "git branch br2"',
+            'git checkout br2',
+            'node ./fH/fileHandler.js "git checkout br2"',
+            'git add .',
+            'git commit -m "some2"',
+            'node ./fH/fileHandler.js "git commit -m \'some2\'"',
+        ];
+    }
+
+    example.forEach((el, ind) => setTimeout(() => {
+        execute(el);
+    }, 1500 * ind));
+})();
