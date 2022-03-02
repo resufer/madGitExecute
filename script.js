8e4bf3e8f49e256238b2e75a9c6b0295f70a4e64
resufer <66846932+resufer@users.noreply.github.com>
2022-03-02 03:01:23 +0300

    Add exp8

--- script.js
+++ script.js
@@ -362,6 +362,189 @@ let execute = (command) => {
             'git add .',
             'git commit -m "master merge f2_sub1 with save conflict"',
         ];
+    } else if (arg === 'example8') {
+        example = [
+            'git init',
+            'git add .',
+            'git commit -m "initial commit"',
+
+
+            'git branch feature1',
+            'git checkout feature1',
+            'git add .',
+            'git commit -m "git commit - checkout feature1"',
+            'git add .',
+            'git commit -m "added feature1"',
+
+                'git branch f1_sub1',
+                'git checkout f1_sub1',
+                'git add .',
+                'git commit -m "git commit - checkout f1_sub1"',
+                'git add .',
+                'git commit -m "added some in f1_sub1"',
+
+                    'git branch f1_sub1_subsub1',
+                    'git checkout f1_sub1_subsub1',
+                    'git add .',
+                    'git commit -m "git commit - checkout f1_sub1_subsub1"',
+                    'git add .',
+                    'git commit -m "added some1 in f1_sub1_subsub1"',
+                    'git add .',
+                    'git commit -m "added some2 in f1_sub1_subsub1"',
+
+                        'git branch f1_sub1_subsub1_subsub1',
+                        'git checkout f1_sub1_subsub1_subsub1',
+                        'git add .',
+                        'git commit -m "git commit - checkout f1_sub1_subsub1_subsub1"',
+                        'git add .',
+                        'git commit -m "added some1 in f1_sub1_subsub1_subsub1"',
+
+                    'git checkout f1_sub1_subsub1',
+                    'git add .',
+                    'git commit -m "git commit - return in f1_sub1_subsub1"',
+
+                'git checkout f1_sub1',
+                'git add .',
+                'git commit -m "git commit - return in f1_sub1"',
+
+            'git checkout feature1',
+            'git add .',
+            'git commit -m "git commit - checkout feature1 after f1_sub1"',
+            'git merge f1_sub1',
+            'git add .',
+            'git commit -m "feature1 merge f1_sub with save conflict"',
+
+                'git branch f1_sub2',
+                'git checkout f1_sub2',
+                'git add .',
+                'git commit -m "git commit - checkout f1_sub2"',
+                'git add .',
+                'git commit -m "added some in f1_sub2"',
+
+            'git checkout feature1',
+            'git add .',
+            'git commit -m "git commit - checkout feature1 after f1_sub2"',
+            
+
+            'git checkout master',
+            'git add .',
+            'git commit -m "return in master after feature1"',
+            'git branch feature2',
+            'git checkout feature2',
+            'git add .',
+            'git commit -m "git commit - checkout feature2"',
+            'git add .',
+            'git commit -m "create recursion in feature2"',
+            'git add .',
+            'git commit -m "update recursion in feature2"',
+
+                'git branch f2_sub1',
+                'git checkout f2_sub1',
+                'git add .',
+                'git commit -m "git commit - checkout f2_sub1"',
+                'git add .',
+                'git commit -m "added some1 in f2_sub1"',
+                'git add .',
+                'git commit -m "added some2 in f2_sub1"',
+
+                    'git branch f2_sub1_subsub1',
+                    'git checkout f2_sub1_subsub1',
+                    'git add .',
+                    'git commit -m "git commit - checkout f2_sub1_subsub1"',
+                    'git add .',
+                    'git commit -m "added some in f2_sub1_subsub1"',
+
+                'git checkout f2_sub1',
+                'git add .',
+                'git commit -m "git commit - return in f2_sub1"',
+
+                    'git branch f2_sub1_subsub2',
+                    'git checkout f2_sub1_subsub2',
+                    'git add .',
+                    'git commit -m "git commit - checkout f2_sub1_subsub2"',
+                    'git add .',
+                    'git commit -m "added some in f2_sub1_subsub2"',
+
+                        'git branch f2_sub1_subsub2_subsubsub1',
+                        'git checkout f2_sub1_subsub2_subsubsub1',
+                        'git add .',
+                        'git commit -m "git commit - checkout f2_sub1_subsub2_subsubsub1"',
+                        'git add .',
+                        'git commit -m "added some1 in f2_sub1_subsub2_subsubsub1"',
+                        'git add .',
+                        'git commit -m "added some2 in f2_sub1_subsub2_subsubsub1"',
+                        'git add .',
+                        'git commit -m "added some3 in f2_sub1_subsub2_subsubsub1"',
+
+                            'git branch f2_sub1_subsub2_subsubsub1_subsubsubsub1',
+                            'git checkout f2_sub1_subsub2_subsubsub1_subsubsubsub1',
+                            'git add .',
+                            'git commit -m "git commit - checkout f2_sub1_subsub2_subsubsub1_subsubsubsub1"',
+
+                        'git checkout f2_sub1_subsub2_subsubsub1',
+                        'git add .',
+                        'git commit -m "git commit - return in f2_sub1_subsub2_subsubsub1"',
+
+                    'git checkout f2_sub1_subsub2',
+                    'git add .',
+                    'git commit -m "git commit - return in f2_sub1_subsub2"',
+
+
+            'git checkout master',
+            'git add .',
+            'git commit -m "return in master after f2_sub1_subsub2"',
+            'git branch feature3',
+            'git checkout feature3',
+            'git add .',
+            'git commit -m "git commit - checkout feature3"',
+            'git add .',
+            'git commit -m "create b-tree in feature3"',
+            'git add .',
+            'git commit -m "update b-tree in feature3"',
+            'git add .',
+            'git commit -m "complete b-tree in feature3"',
+            'git merge f2_sub1_subsub2_subsubsub1_subsubsubsub1',
+            'git add .',
+            'git commit -m "feature3 merge f2_sub1_subsub2_subsubsub1_subsubsubsub1 with save conflict"',
+
+
+            'git checkout feature2',
+            'git add .',
+            'git commit -m "return in branch 2"',
+            'git merge feature3',
+            'git add .',
+            'git commit -m "feature2 merge feature3 with save conflict"',
+
+
+            'git checkout feature1',
+            'git add .',
+            'git commit -m "return in branch 1"',
+            'git merge feature2',
+            'git add .',
+            'git commit -m "feature1 merge feature2 with save conflict"',
+
+
+            'git checkout master',
+            'git add .',
+            'git commit -m "return in master"',
+            'git merge feature1',
+            'git add .',
+            'git commit -m "master merge feature1 with save conflict"',
+
+            
+            'git merge f1_sub1',
+            'git add .',
+            'git commit -m "master merge f1_sub1 with save conflict"',
+            'git merge f1_sub2',
+            'git add .',
+            'git commit -m "master merge f1_sub2 with save conflict"',
+            'git merge f2_sub1_subsub1',
+            'git add .',
+            'git commit -m "master merge f2_sub1_subsub1 with save conflict"',
+            'git merge f2_sub1',
+            'git add .',
+            'git commit -m "master merge f2_sub1 with save conflict"',
+        ];
     }
 
     clear();
